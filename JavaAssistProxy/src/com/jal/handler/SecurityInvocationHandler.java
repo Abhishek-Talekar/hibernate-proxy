cfcfc0614456113df469d3ead82fcb7775ed27b5
Abhishek Talekar <talekarabhishek02@gmail.com>
2020-02-22 23:46:45 +0530

    Not Executing Commit

--- JavaAssistProxy/src/com/jal/handler/SecurityInvocationHandler.java
+++ JavaAssistProxy/src/com/jal/handler/SecurityInvocationHandler.java
@@ -3,14 +3,17 @@ package com.jal.handler;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
-public class SecurityInvocationHandler implements InvocationHandler{
+public class SecurityInvocationHandler implements InvocationHandler {
 
 	@Override
 	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-		Object obj;
-		if(method.getName().equals("getBalance")) {
+		Class clazz = null;
+		Object obj = null;
+		if (method.getName().equals("getBalance")) {
 			System.out.println("Security Verified");
-			
+			method.invoke(proxy, args);
+			clazz.getClass().isInstance(obj);
+			return obj;
 		}
 		return null;
 	}
---
+++ JavaAssistProxy/src/com/jal/test/ProxyTest.java
@@ -0,0 +1,17 @@
+package com.jal.test;
+
+import java.lang.reflect.Proxy;
+
+import com.jal.handler.SecurityInvocationHandler;
+import com.jal.services.AccountServiceImpl;
+import com.jal.services.IAccountService;
+
+public class ProxyTest {
+	public static void main(String[] args) {
+		SecurityInvocationHandler handler = new SecurityInvocationHandler();
+		IAccountService proxy = (IAccountService) Proxy.newProxyInstance(IAccountService.class.getClassLoader(), new Class[] {IAccountService.class}, handler);
+		//AccountServiceImpl accountServiceImpl = new AccountServiceImpl();
+		double balance = proxy.getBalance("2209", "Close");
+		System.out.println(balance);
+	}
+}
